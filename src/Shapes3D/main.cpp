43e5af02528d71c4a4591a6cd3a549dcfc5055cb
Eric Tuvesson <eric.tuvesson@gmail.com>
2017-01-04 17:52:47 +0100

    Update Shapes3D

--- src/Shapes3D/main.cpp
+++ src/Shapes3D/main.cpp
@@ -1,11 +1,8 @@
 
 /*
-* Written by Eric Tuvesson
-*
-* The OpenGL rendering is based on NeHe's Tutorial '3D Shapes'
-* which can be found here http://nehe.gamedev.net/tutorial/3d_shapes/10035/
-*
-*/
+ * The OpenGL rendering is based on NeHe's Tutorial '3D Shapes'
+ * which can be found here http://nehe.gamedev.net/tutorial/3d_shapes/10035/
+ */
 
 #include <iostream>
 
@@ -15,51 +12,65 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 
+#define SCREEN_WIDTH	800
+#define SCREEN_HEIGHT	600
+#define WINDOW_TITLE	"SDL2 OpenGL 3D Shapes"
+
+#define OPENGL_MAJOR	2
+#define OPENGL_MINOR	1
+
+// Output the message and pause the console if necessary.
+void PrintError(const char* str);
+
 int main(int argc, char *argv[])
 {
 	SDL_Window *window;
 	SDL_GLContext context;
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) != NULL)
 	{
-		std::cout << SDL_GetError() << std::endl;
-		return 1;
+		PrintError(SDL_GetError());
+		return EXIT_FAILURE;
 	}
 
-	// Lets assume that the Client can run OpenGL 2.1
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+	// Set the desired OpenGL version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, OPENGL_MAJOR);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, OPENGL_MINOR);
 
-	window = SDL_CreateWindow("SDL2 OpenGL 3D Shapes",
+	// Create the window using SDL
+	window = SDL_CreateWindow(WINDOW_TITLE,
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		800, 600, SDL_WINDOW_OPENGL);
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
 
-	if (window == 0)
+	if (window == NULL)
 	{
-		std::cout << SDL_GetError() << std::endl;
-		return 1;
+		PrintError(SDL_GetError());
+		return EXIT_FAILURE;
 	}
 
+	// Create the OpenGL context for the window using SDL
 	context = SDL_GL_CreateContext(window);
-
-	if (context == 0)
+	if (context == NULL)
 	{
-		std::cout << SDL_GetError() << std::endl;
-		return 1;
+		PrintError(SDL_GetError());
+		return EXIT_FAILURE;
 	}
 
+	// Enable glew experimental, this enables some more OpenGL extensions.
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
 	{
-		std::cout << "Failed to initialize GLEW" << std::endl;
-		return 1;
+		PrintError("Failed to initialize GLEW");
+		return EXIT_FAILURE;
 	}
 
-	// OpenGL & SDL2 Initialized
-
+	// Application Variables
 	GLfloat anglePyramid = 0.0f;
 	GLfloat angleCube = 0.0f;
+	bool isRunning = true;
 
+	// Set some OpenGL settings
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClearDepth(1.0f);
 	glEnable(GL_DEPTH_TEST);
@@ -67,118 +78,146 @@ int main(int argc, char *argv[])
 	glShadeModel(GL_SMOOTH);
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
-	GLfloat aspect = (GLfloat)800 / (GLfloat)600;
-	glViewport(0, 0, 800, 600);
+	GLfloat aspect = (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT;
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
 
-	// 
-
-	bool isRunning = true;
 	SDL_Event event;
 	while (isRunning)
 	{
 		while (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_QUIT)
+			switch (event.type)
+			{
+			case SDL_QUIT: 
 				isRunning = false;
+				break;
+			}
 		}
 
+		// Rotate the shapes
+		anglePyramid += 0.2f;
+		angleCube -= 0.15f;
+
+		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		glMatrixMode(GL_MODELVIEW);
 
+		// Reset the current transform
 		glLoadIdentity();
+
+		// Set the cube location
 		glTranslatef(1.5f, 0.0f, -7.0f);
 		glRotatef(angleCube, 1.0f, 1.0f, 1.0f);
 
+		// Draw the cube
 		glBegin(GL_QUADS);
-		// Top face (y = 1.0f)
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, 1.0f, -1.0f);
-		glVertex3f(-1.0f, 1.0f, -1.0f);
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(1.0f, 1.0f, 1.0f);
+		{
+			// Top face (y = 1.0f)
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(1.0f, 1.0f, -1.0f);
+			glVertex3f(-1.0f, 1.0f, -1.0f);
+			glVertex3f(-1.0f, 1.0f, 1.0f);
+			glVertex3f(1.0f, 1.0f, 1.0f);
 
-		// Bottom face (y = -1.0f)
-		glColor3f(1.0f, 0.5f, 0.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
+			// Bottom face (y = -1.0f)
+			glColor3f(1.0f, 0.5f, 0.0f);
+			glVertex3f(1.0f, -1.0f, 1.0f);
+			glVertex3f(-1.0f, -1.0f, 1.0f);
+			glVertex3f(-1.0f, -1.0f, -1.0f);
+			glVertex3f(1.0f, -1.0f, -1.0f);
 
-		// Front face  (z = 1.0f)
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
+			// Front face  (z = 1.0f)
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(1.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f, -1.0f, 1.0f);
+			glVertex3f(1.0f, -1.0f, 1.0f);
 
-		// Back face (z = -1.0f)
-		glColor3f(1.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-		glVertex3f(-1.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, -1.0f);
+			// Back face (z = -1.0f)
+			glColor3f(1.0f, 1.0f, 0.0f);
+			glVertex3f(1.0f, -1.0f, -1.0f);
+			glVertex3f(-1.0f, -1.0f, -1.0f);
+			glVertex3f(-1.0f, 1.0f, -1.0f);
+			glVertex3f(1.0f, 1.0f, -1.0f);
 
-		// Left face (x = -1.0f)
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, 1.0f);
-		glVertex3f(-1.0f, 1.0f, -1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
+			// Left face (x = -1.0f)
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(-1.0f, 1.0f, 1.0f);
+			glVertex3f(-1.0f, 1.0f, -1.0f);
+			glVertex3f(-1.0f, -1.0f, -1.0f);
+			glVertex3f(-1.0f, -1.0f, 1.0f);
 
-		// Right face (x = 1.0f)
-		glColor3f(1.0f, 0.0f, 1.0f);
-		glVertex3f(1.0f, 1.0f, -1.0f);
-		glVertex3f(1.0f, 1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
+			// Right face (x = 1.0f)
+			glColor3f(1.0f, 0.0f, 1.0f);
+			glVertex3f(1.0f, 1.0f, -1.0f);
+			glVertex3f(1.0f, 1.0f, 1.0f);
+			glVertex3f(1.0f, -1.0f, 1.0f);
+			glVertex3f(1.0f, -1.0f, -1.0f);
+		}
 		glEnd();
 
+		// Reset the current transform
 		glLoadIdentity();
+
+		// Set the pyramid location
 		glTranslatef(-1.5f, 0.0f, -6.0f);
 		glRotatef(anglePyramid, 1.0f, 1.0f, 0.0f);
 
+		// Draw the pyramid
 		glBegin(GL_TRIANGLES);
-		// Front
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
+		{
+			// Front
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(0.0f, 1.0f, 0.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(-1.0f, -1.0f, 1.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(1.0f, -1.0f, 1.0f);
 
-		// Right
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(1.0f, -1.0f, 1.0f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
+			// Right
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(0.0f, 1.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(1.0f, -1.0f, 1.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(1.0f, -1.0f, -1.0f);
 
-		// Back
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(1.0f, -1.0f, -1.0f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
+			// Back
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(0.0f, 1.0f, 0.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(1.0f, -1.0f, -1.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(-1.0f, -1.0f, -1.0f);
 
-		// Left
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 1.0f, 0.0f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(-1.0f, -1.0f, -1.0f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f, -1.0f, 1.0f);
+			// Left
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(0.0f, 1.0f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(-1.0f, -1.0f, -1.0f);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(-1.0f, -1.0f, 1.0f);
+		}
 		glEnd();
 
+		// Swap the OpenGL window buffers
 		SDL_GL_SwapWindow(window);
-
-		anglePyramid += 0.2f;
-		angleCube -= 0.15f;
 	}
 
-	return 0;
+	// Release resources
+
+	return EXIT_SUCCESS;
+}
+
+void PrintError(const char* str)
+{
+	std::cout << str << '\n';
+
+#ifdef _DEBUG
+	std::cin.get();
+#endif
 }
